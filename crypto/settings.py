f3c7c07b4a761f471a7b36b0124489c682a32af0
Ndagi Stanley <ndagis@gmail.com>
2019-03-26 20:02:12 +0300

    Add Sendgrid

--- crypto/settings.py
+++ crypto/settings.py
@@ -125,12 +125,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # comment the following if testing locally
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
